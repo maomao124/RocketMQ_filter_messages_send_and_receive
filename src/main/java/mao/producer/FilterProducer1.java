73bf60e4ad8fd0ed22ce42f65a0c558d16cae11b
mao <1296193245@qq.com>
2022-12-07 14:16:23 +0800

    RocketMQ_filter_messages_send_and_receive

---
+++ src/main/java/mao/producer/FilterProducer1.java
@@ -0,0 +1,19 @@
+package mao.producer;
+
+/**
+ * Project name(项目名称)：RocketMQ_过滤消息的发送与接收
+ * Package(包名): mao.producer
+ * Class(类名): FilterProducer1
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/12/7
+ * Time(创建时间)： 14:16
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class FilterProducer1
+{
+
+}
